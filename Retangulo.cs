b55b354269861395c6a8f989c1ce562eb2ecf432
Felipe Marins <marins.sgs@gmail.com>
2021-04-29 22:57:07 -0300

    Criar classe Retangulo

---
+++ Retangulo.cs
@@ -0,0 +1,65 @@
+using System;
+
+public class Retangulo : Figura
+{
+    protected double _altura;
+    protected double _largura;
+    public double Altura
+    {
+        get { return this._altura; }
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Valor inválido para altura!");
+            }
+            else
+            {
+                this._altura = value;
+                if (this._largura > 0)
+                {
+                    this.AtualizarArea();
+                    this.AtualizarPerimetro();
+                }
+            }
+        }
+    }
+    public double Largura
+    {
+        get { return this._largura; }
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Valor inválido para largura!");
+            }
+            else
+            {
+                this._largura = value;
+                if (this._altura > 0)
+                {
+                    this.AtualizarArea();
+                    this.AtualizarPerimetro();
+                }
+            }
+        }
+    }
+
+    public Retangulo(double altura, double largura)
+    {
+        this.Altura = altura;
+        this.Largura = largura;
+        this.AtualizarArea();
+        this.AtualizarPerimetro();
+    }
+
+    public void AtualizarArea()
+    {
+        this._area = this._altura * this._largura;
+    }
+
+    public void AtualizarPerimetro()
+    {
+        this._perimetro = this._altura * 2 + this._largura * 2;
+    }
+}
