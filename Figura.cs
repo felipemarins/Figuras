78d506134dc797a0d7aba5e05f22b536e276ab52
Felipe Marins <marins.sgs@gmail.com>
2021-04-29 23:05:49 -0300

    Adicionar propriedade NomeDaFigura

--- Figura.cs
+++ Figura.cs
@@ -2,9 +2,14 @@ using System;
 
 public class Figura
 {
+    protected string _nomeDaFigura;
     protected double _area;
     protected double _perimetro;
-    
+
+    public string NomeDaFigura
+    {
+        get { return this._nomeDaFigura; }
+    }
     public double Area
     {
         get { return this._area; }
@@ -16,6 +21,7 @@ public class Figura
 
     public void ImprimirFigura()
     {
+        Console.WriteLine("Nome da figura: {0}", this.NomeDaFigura);
         Console.WriteLine("Area: {0}", this.Area);
         Console.WriteLine("Perimetro: {0}", this.Perimetro);
     }
