18d13ae7655cb498ef60155e4e8a9e5663e9f0b5
Felipe Marins <marins.sgs@gmail.com>
2021-04-29 22:17:49 -0300

    Criar classe Figura

---
+++ Figura.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Figura
+{
+    protected double _area;
+    protected double _perimetro;
+    public double Area
+    {
+        get { return this._area; }
+    }
+    public double Perimetro
+    {
+        get { return this._perimetro; }
+    }
+}
